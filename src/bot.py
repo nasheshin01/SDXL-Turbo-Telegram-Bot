f5e8908e74d3cc7340ae4bd3256f1cb911dfd55b
Nikolay Sheshin <nasheshin.portfolio@gmail.com>
2023-12-16 19:05:20 +0500

    Added all comands

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,7 @@
 import os
 import telebot
 
-from sdxl_turbo import SdxlTurbo
+from sdxl_turbo import SdxlTurbo, SdxlQuery
 
 
 def read_config():
@@ -16,18 +16,85 @@ def read_config():
         return cfg_dict
 
 
+queries = {}
 
+
+def generate_and_send_image(chat_id):
+    image_stream = generator.generate(queries[chat_id])
+    bot.send_photo(chat_id, photo=image_stream)
+    
 
 cfg = read_config()
 bot = telebot.TeleBot(cfg['token'])
 generator = SdxlTurbo()
 
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    image_stream = generator.generate(message.text)
-    bot.send_photo(message.chat.id, photo=image_stream)
+@bot.message_handler(commands=['start'])
+def start_session(message):
+    queries[message.chat.id] = SdxlQuery("beatiful face of bear")
+    generate_and_send_image(message.chat.id)
+    bot.reply_to(message, "INFO") # TODO: add info
 
+@bot.message_handler(commands=['set'])
+def set_main_prompt(message):
+    set_prompt = ' '.join(message.text.split()[1:])
+    chat_id = message.chat.id
+    if not chat_id in queries:
+        queries[chat_id] = SdxlQuery(set_prompt)
+    else:
+        queries[chat_id].main_prompt = set_prompt
+
+    bot.send_message(chat_id, f'Main prompt was set to "{set_prompt}"')
+    generate_and_send_image(chat_id)
+    
+@bot.message_handler(commands=['session_info'])
+def session_info(message):
+    chat_id = message.chat.id
+    if not chat_id in queries:
+        queries[chat_id] = SdxlQuery("beatiful face of bear")
+
+    bot.send_message(chat_id, queries[chat_id].query_info())
+
+@bot.message_handler(commands=['new_seed'])
+def new_seed(message):
+    chat_id = message.chat.id
+    if not chat_id in queries:
+        queries[chat_id] = SdxlQuery("beatiful face of bear")
+
+    queries[chat_id].new_seed()
+    bot.send_message(chat_id, "Creating image with new seed...")
+    generate_and_send_image(chat_id)
+
+@bot.message_handler(commands=['add'])
+def add_tag(message):
+    tag = ' '.join(message.text.split()[1:])
+    chat_id = message.chat.id
+    if not chat_id in queries:
+        queries[chat_id] = SdxlQuery("beatiful face of bear")
+
+    
+    queries[chat_id].add_tag(tag)
+    bot.send_message(chat_id, "Creating image with new seed...")
+    generate_and_send_image(chat_id)
+
+@bot.message_handler(commands=['remove'])
+def remove_tag(message):
+    chat_id = message.chat.id
+    if not chat_id in queries:
+        queries[chat_id] = SdxlQuery("beatiful face of bear")
+
+    message_split = message.text.split()[1:]
+    if len(message_split) == 0:
+        is_removed = queries[chat_id].try_remove_last_tag()
+    else:
+        is_removed = queries[chat_id].try_remove_tag(' '.join(message_split))
+
+    if not is_removed:
+        bot.send_message(chat_id, "No tags or tag was not found")
+        return
+    
+    bot.send_message(chat_id, "Tag removed. Generating new image...")
+    generate_and_send_image(chat_id)
 
 
 bot.infinity_polling()
--- src/sdxl_turbo.py
+++ src/sdxl_turbo.py
@@ -1,9 +1,47 @@
 import torch
 
 from diffusers import AutoPipelineForText2Image
-from PIL import Image
 from io import BytesIO
+from random import randint
 
+
+class SdxlQuery:
+
+    def __init__(self, main_prompt: str) -> None:
+        self.main_prompt = main_prompt
+        self.seed = randint(0, 100000)
+        self.tags = ['vectorized image', '2d', 'high resolution', 'powerpoint style']
+
+    def get_full_prompt(self) -> str:
+        return ', '.join([self.main_prompt, *self.tags])
+    
+    def query_info(self):
+        main_prompt_info = f"Main prompt: {self.main_prompt}"
+        tags_info = 'Tags: ' + ', '.join(self.tags)
+        seed_info = f'Random seed: {self.seed}'
+
+        return '\n'.join([main_prompt_info, tags_info, seed_info])
+
+    def add_tag(self, tag: str):
+        self.tags.append(tag)
+
+    def try_remove_tag(self, tag: str) -> bool:
+        if tag in self.tags:
+            self.tags.remove(tag)
+            return True
+        
+        return False
+    
+    def try_remove_last_tag(self) -> bool:
+        if len(self.tags) == 0:
+            return False
+        
+        self.tags = self.tags[:-1]
+        return True
+        
+    def new_seed(self):
+        self.seed = randint(0, 100000)
+        
 
 class SdxlTurbo:
 
@@ -11,8 +49,10 @@ class SdxlTurbo:
         self.pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
         self.pipe.to("cuda")
 
-    def generate(self, prompt: str) -> BytesIO:
-        image = self.pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+    def generate(self, query: SdxlQuery) -> BytesIO:
+        generator = [torch.Generator(device="cuda").manual_seed(query.seed)]
+        prompt = query.get_full_prompt()
+        image = self.pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0, generator=generator).images[0]
 
         image_bytes_stream = BytesIO()
         image_bytes_stream.name = 'tmp.jpeg'
