66420edc954d1ebac636fdda0aa83e51a20c9966
Nikolay Sheshin <nasheshin.portfolio@gmail.com>
2023-12-20 22:08:48 +0500

    Updated README.md and comments

--- src/bot.py
+++ src/bot.py
@@ -7,6 +7,7 @@ from sdxl_turbo import SdxlTurbo, SdxlQuery
 DEFAULT_MAIN_PROMPT = "beatiful face of bear"
 
 
+# Функция чтения конфига для получения информации о токене бота
 def read_config():
     with open('src\\config.cfg', 'r') as cfg:
         cfg_lines = cfg.readlines()
@@ -21,11 +22,13 @@ def read_config():
 
 queries = {}
 
+# Функция для проверки того, что сессия для юзера запущена, если нет - создаем сессию
 def check_and_create_new_query_if_necessary(chat_id, main_prompt=DEFAULT_MAIN_PROMPT):
     if not chat_id in queries:
         queries[chat_id] = SdxlQuery(main_prompt)
 
 
+# Функция для генерации и отправки изображения
 def generate_and_send_image(chat_id):
     image_stream = generator.generate(queries[chat_id])
     bot.send_photo(chat_id, photo=image_stream)
@@ -36,6 +39,7 @@ bot = telebot.TeleBot(cfg['token'])
 generator = SdxlTurbo()
 
 
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start_session(message):
     chat_id = message.chat.id
@@ -50,6 +54,7 @@ def start_session(message):
                      "/remove [optional]{tag} - removes written tag or the last (if command used without argument)\n" +
                      "/new_seed - generates new random number, to randomize images")
 
+# Обработчик команды /set
 @bot.message_handler(commands=['set'])
 def set_main_prompt(message):
     set_prompt = ' '.join(message.text.split()[1:])
@@ -59,12 +64,14 @@ def set_main_prompt(message):
     bot.send_message(chat_id, f'Main prompt was set to "{set_prompt}"')
     generate_and_send_image(chat_id)
     
+# Обработчик команды /session_info
 @bot.message_handler(commands=['session_info'])
 def session_info(message):
     chat_id = message.chat.id
     check_and_create_new_query_if_necessary(chat_id)
     bot.send_message(chat_id, queries[chat_id].query_info())
 
+# Обработчик команды /new_seed
 @bot.message_handler(commands=['new_seed'])
 def new_seed(message):
     chat_id = message.chat.id
@@ -73,6 +80,7 @@ def new_seed(message):
     bot.send_message(chat_id, "Creating image with new seed...")
     generate_and_send_image(chat_id)
 
+# Обработчик команды /add
 @bot.message_handler(commands=['add'])
 def add_tag(message):
     tag = ' '.join(message.text.split()[1:])
@@ -84,11 +92,13 @@ def add_tag(message):
     bot.send_message(chat_id, "Creating image with new tags...")
     generate_and_send_image(chat_id)
 
+# Обработчик команды /remove
 @bot.message_handler(commands=['remove'])
 def remove_tag(message):
     chat_id = message.chat.id
     check_and_create_new_query_if_necessary(chat_id)
 
+    # Здесь проверяем задан ли аргумент у команды
     message_split = message.text.split()[1:]
     if len(message_split) == 0:
         is_removed = queries[chat_id].try_remove_last_tag()
@@ -103,6 +113,7 @@ def remove_tag(message):
     generate_and_send_image(chat_id)
 
 
+# Запуск бота для бесконечной обработки ввода в бот
 bot.infinity_polling()
 
 if __name__ == "__main__":
