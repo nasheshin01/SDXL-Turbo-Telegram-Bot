fe58ac36e8537f0c5b6d2697c917b6b67aec91e5
Nikolay Sheshin <nasheshin.portfolio@gmail.com>
2023-12-20 21:01:46 +0500

    Added full functionality

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,9 @@ import telebot
 from sdxl_turbo import SdxlTurbo, SdxlQuery
 
 
+DEFAULT_MAIN_PROMPT = "beatiful face of bear"
+
+
 def read_config():
     with open('src\\config.cfg', 'r') as cfg:
         cfg_lines = cfg.readlines()
@@ -18,6 +21,10 @@ def read_config():
 
 queries = {}
 
+def check_and_create_new_query_if_necessary(chat_id, main_prompt=DEFAULT_MAIN_PROMPT):
+    if not chat_id in queries:
+        queries[chat_id] = SdxlQuery(main_prompt)
+
 
 def generate_and_send_image(chat_id):
     image_stream = generator.generate(queries[chat_id])
@@ -31,36 +38,37 @@ generator = SdxlTurbo()
 
 @bot.message_handler(commands=['start'])
 def start_session(message):
-    queries[message.chat.id] = SdxlQuery("beatiful face of bear")
+    chat_id = message.chat.id
+    queries[chat_id] = SdxlQuery(DEFAULT_MAIN_PROMPT)
     generate_and_send_image(message.chat.id)
-    bot.reply_to(message, "INFO") # TODO: add info
+    bot.send_message(chat_id, "Welcome to this student helper app for generating images!")
+    bot.send_message(chat_id, "Main commands:\n" +
+                     "/start - starts session and gives some helphul info\n" +
+                     "/session_info - gives information about parameters of current session\n" +
+                     "/set {main_prompt} - sets new main prompt for generating\n" +
+                     "/add {tag} - adds new tag for generating\n" +
+                     "/remove [optional]{tag} - removes written tag or the last (if command used without argument)\n" +
+                     "/new_seed - generates new random number, to randomize images")
 
 @bot.message_handler(commands=['set'])
 def set_main_prompt(message):
     set_prompt = ' '.join(message.text.split()[1:])
     chat_id = message.chat.id
-    if not chat_id in queries:
-        queries[chat_id] = SdxlQuery(set_prompt)
-    else:
-        queries[chat_id].main_prompt = set_prompt
-
+    check_and_create_new_query_if_necessary(chat_id)
+    queries[chat_id].main_prompt = set_prompt
     bot.send_message(chat_id, f'Main prompt was set to "{set_prompt}"')
     generate_and_send_image(chat_id)
     
 @bot.message_handler(commands=['session_info'])
 def session_info(message):
     chat_id = message.chat.id
-    if not chat_id in queries:
-        queries[chat_id] = SdxlQuery("beatiful face of bear")
-
+    check_and_create_new_query_if_necessary(chat_id)
     bot.send_message(chat_id, queries[chat_id].query_info())
 
 @bot.message_handler(commands=['new_seed'])
 def new_seed(message):
     chat_id = message.chat.id
-    if not chat_id in queries:
-        queries[chat_id] = SdxlQuery("beatiful face of bear")
-
+    check_and_create_new_query_if_necessary(chat_id)
     queries[chat_id].new_seed()
     bot.send_message(chat_id, "Creating image with new seed...")
     generate_and_send_image(chat_id)
@@ -69,19 +77,17 @@ def new_seed(message):
 def add_tag(message):
     tag = ' '.join(message.text.split()[1:])
     chat_id = message.chat.id
-    if not chat_id in queries:
-        queries[chat_id] = SdxlQuery("beatiful face of bear")
+    check_and_create_new_query_if_necessary(chat_id)
 
     
     queries[chat_id].add_tag(tag)
-    bot.send_message(chat_id, "Creating image with new seed...")
+    bot.send_message(chat_id, "Creating image with new tags...")
     generate_and_send_image(chat_id)
 
 @bot.message_handler(commands=['remove'])
 def remove_tag(message):
     chat_id = message.chat.id
-    if not chat_id in queries:
-        queries[chat_id] = SdxlQuery("beatiful face of bear")
+    check_and_create_new_query_if_necessary(chat_id)
 
     message_split = message.text.split()[1:]
     if len(message_split) == 0:
